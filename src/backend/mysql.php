43a3ee166f3fef858e1a482a1a1bdff2635137f1
Dale Moore <dmoore092@gmail.com>
2019-02-04 21:20:18 -0500

    fixed the db/vagrant files

--- src/backend/mysql.php
+++ src/backend/mysql.php
@@ -5,50 +5,34 @@
     $dbpass = 'root';
     $dbhost = 'localhost';
 
-    $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-    mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-
-
-    //var_dump($_GET["name"]);
-    //$name = $_GET["name"];
-    //$name = $_POST["name"];
-    //$name = "dale";
-    // $db_host = "127.0.0.1";
-    // $db_user = "root";
-    // $db_pass = "root";
-    // $db_name = "kriskringle";
-    //echo "test1!";
-    //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    // $mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-    //$connect = mysqli_connect("127.0.0.1", "root", "root", "mysql");
-    //echo "test1!";
-    //var_dump($mysqli);
+    $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to Connect to '$dbhost'");
     
-//     if(!$mysqli){
-//         echo "connection error: " . mysqli_connect_error();
-//         die();
-//     }
-//     else{
-//         echo "success!";
-//         //$query = "SELECT * FROM preferences WHERE name ='". $name."';";
-//         $query = "SELECT * FROM preferences;";
-//         $result = mysqli_query($mysqli, $query);
-//         if($result){
-//             //echo $result["name"];
-//             $arr = array();
+    if(!$mysqli){
+        echo "connection error: " . mysqli_connect_error();
+        die();
+    }
+    else{
+        //$query = "SELECT * FROM preferences WHERE name ='". $name."';";
+        $query = "SELECT * FROM preferences;";
+        // $result = mysqli_query($mysqli, $query);
+        $result = mysqli_query($mysqli, "select * from preferences;");
+        //echo mysqli_error($mysqli);
+        //var_dump($result);
+        if($result){
+            $arr = array();
             
-//             while($row = mysqli_fetch_assoc($result)){
-//                 $myObj = new stdClass();
-//                 //echo $row["name"] ." ". $row["pref1"] ." ".  $row["pref2"] ." ".  $row["pref3"];
-//                 $myObj->name = $row["name"];
-//                 $myObj->pref1 = $row["pref1"];
-//                 $myObj->pref2 = $row["pref2"];
-//                 $myObj->pref3 = $row["pref3"];
-//                 array_push($arr, $myObj);
-//             }
-//         }
-//         echo json_encode($arr);
-//     }
+            while($row = mysqli_fetch_assoc($result)){
+                $myObj = new stdClass();
+                //echo $row["name"] ." ". $row["pref1"] ." ".  $row["pref2"] ." ".  $row["pref3"];
+                $myObj->name = $row["name"];
+                $myObj->pref1 = $row["pref1"];
+                $myObj->pref2 = $row["pref2"];
+                $myObj->pref3 = $row["pref3"];
+                array_push($arr, $myObj);
+            }
+        }
+        echo json_encode($arr);
+    }
 
 
 
